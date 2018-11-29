bf379b3e7d2c2197db58f8bbcc559ea1c4b10681
keprom <kokshetau.energo.prom@mail.ru>
2018-11-29 16:26:05 +0600

    added notification time

--- system/application/controllers/billing.php
+++ system/application/controllers/billing.php
@@ -89,6 +89,7 @@ class Billing extends Controller
 		$data['poisk']=$this->session->userdata('poisk');
 		if ($this->session->userdata('poisk')==NULL) $data['poisk']='1';
 		$this->load->view("left",$data);
+		$this->load->view("messages");		
 	}
 	function phpinfo()
 	{ 
@@ -2316,6 +2317,8 @@ function oplata_import()
     function perehod()
 	{
 		$this->db->query("select industry.goto_next_period_fine();");
+		$array = array(1 => 'Переход в следующий месяц прошел успешно!');
+		$this->session->set_flashdata('success', $array);		
 		redirect("billing");
 	}
 	
