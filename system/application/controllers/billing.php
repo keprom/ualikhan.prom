4a918f3322ecc4fa3d41ffc85cb12247ac687ed8
keprom <kokshetau.energo.prom@mail.ru>
2018-12-05 11:17:23 +0600

    added nach to 1C 8.3

--- system/application/controllers/billing.php
+++ system/application/controllers/billing.php
@@ -1057,49 +1057,42 @@ class Billing extends Controller
             echo "DBF file is busy!";
         }
     }
-	
-	function perenos_nach()
-	{
-		$nach=$this->db->get("industry.schetfactura_to_1c");
-		
-		set_time_limit(0);
-		$db = dbase_open("c:/oplata/schet.dbf", 2);
-		
-		if ($db)
-		{			
-			for ($i=1;$i<dbase_numrecords($db)+1;$i++)
-			{
-				dbase_delete_record($db, $i);	
-			}
-			dbase_pack($db);
-			dbase_close($db);
-			
-			$db2 = dbase_open("c:/oplata/schet.dbf", 2);
-			foreach ($nach->result() as $n)
-			{
-				dbase_add_record($db2,
-				array (
-				$n->dog,
-						$n->kvt,
-						$n->tarif,$n->beznds,$n->nds,$n->snds, $n->nomer,$this->d2($n->data), "0".$n->dog1
-				
-					)
-					);
-			}
-				
-			
-			dbase_close($db2);			
-		}
-		else 
-			echo "База не открыта";		
-	}
-	
-	
-	
-	
-	
-	
-	function pre_schetoplata()
+
+    function perenos_nach()
+    {
+        set_time_limit(0);
+        @$db = dbase_open("c:/oplata/schet.dbf", 2);
+        if ($db) {
+            $this->db->where('period_id', $this->get_cpi());
+            $nach = $this->db->get("industry.schetfactura_to_1c");
+            for ($i = 1; $i < dbase_numrecords($db) + 1; $i++) {
+                dbase_delete_record($db, $i);
+            }
+            dbase_pack($db);
+            dbase_close($db);
+            $db2 = dbase_open("c:/oplata/schet.dbf", 2);
+            foreach ($nach->result() as $n) {
+                dbase_add_record($db2,
+                    array(
+                        $n->dog,
+                        $n->kvt,
+                        $n->tarif, $n->beznds, $n->nds, $n->snds, $n->nomer, $this->d2($n->data), "0" . $n->dog1
+                    )
+                );
+            }
+            dbase_close($db2);
+            $array = array(1 => 'Перенос прошел успешно!');
+            $this->session->set_flashdata('success', $array);
+            redirect('billing/pre_perehod');
+        } else {
+            $array = array(1 => 'Перенос не возможен. Закройте файл schet.dbf!');
+            $this->session->set_flashdata('error', $array);
+            redirect('billing/pre_perehod');
+        }
+    }
+
+
+    function pre_schetoplata()
 	{
 		$this->db->where('id',$this->uri->segment(3));
 		$data['r']= $this->	db->get('industry.firm');
